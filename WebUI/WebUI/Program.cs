6d8f2d3381b20cb0fa2fb0de89ab0293c5939e34
Paulo César <pcmorais7@gmail.com>
2024-11-26 00:10:18 -0300

    Tratamento encoding e globalization

--- WebUI/WebUI/Program.cs
+++ WebUI/WebUI/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.WebEncoders;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
+builder.Services.Configure<WebEncoderOptions>(options =>
+{
+    options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +22,17 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var cultureInfo = new System.Globalization.CultureInfo("pt-BR");
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(cultureInfo),
+    SupportedCultures = new[] { cultureInfo },
+    SupportedUICultures = new[] { cultureInfo }
+});
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
